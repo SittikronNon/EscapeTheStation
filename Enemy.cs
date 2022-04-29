3d5d9394b60eeafc35454e0226b3c8c35886c52e
SittikronNon <sittikron.lamulsab@gmail.com>
2022-04-29 20:04:41 +0700

    first commit

---
+++ Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Enemy : MonoBehaviour
+{
+    //enemy hit points(can change it to whatever you want Kappa)
+    public int health = 100;
+
+    
+    public GameObject deathEffect;
+
+
+    
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# game-coding
+this code is only for learning on how to use the github
+so i don't even know a thing
